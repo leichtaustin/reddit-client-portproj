70bc16d4b69c1a0181d328d15ee08c5fd37bef45
Austin Leicht <leichtaustin@gmail.com>
2023-07-24 15:22:15 -0600

    tested functionality, reverting

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 //import items here
 import { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-reducx';
+import { useDispatch, useSelector } from 'react-redux';
 import { setSearchTerm } from '../../store/redditSlice';
 
 
